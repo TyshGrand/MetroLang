0ff53fa56675161cc95f2c36c3e352bdcb901a7d
TyshGrand <tushargupta9041@gmail.com>
2025-04-08 18:51:53 +0530

    Abandond having a flutter FE and add that to backlogs

--- frontend/lib/styles/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MetroLangColors{
-
-  static const Color primary = Color(0xFF5F59FF);
-  static const Color mainBackground = Colors.black87;
-  static const Color mainTitle = Colors.white;
-  static const Color hintText = Color(0xffa1a1a1);
-}
